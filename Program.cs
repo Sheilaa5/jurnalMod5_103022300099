95ca8080018ff416912513b46e4831489d527342
PRAKTIKAN <PRAKTIKAN@DESKTOP-MU6PA4D>
2025-03-20 11:12:27 +0700

    impelementasi-generic-class

--- Program.cs
+++ Program.cs
@@ -1,6 +1,34 @@
 ﻿using System;
 
-public class SimpleDataBase
+public class SimpleDataBase<T>
 {
+    private List<T> StoredDataBase;
+    private List<DateTime> inputDates;
 
+    public void AddNewData(T data)
+    {
+        StoredDataBase.Add(data);
+        inputDates.Add(DateTime.Now);
+    }
+
+    public void PrintAllData()
+    {
+        for (int i = 0; i < StoredDataBase.Count; i++)
+        {
+            Console.WriteLine("Data: " + (i + 1) + "berisi " + StoredDataBase[i] + "yang disimpan pada waktu UTC: " + inputDates[i]);
+        }
+
+    }
+}
+
+public class program
+{
+    public static void Main(string[] args)
+    {
+        SimpleDataBase<long> data = new SimpleDataBase<long>();
+        data.AddNewData(10);
+        data.AddNewData(20);
+        data.AddNewData(30);
+        data.PrintAllData();
+    }
 }
